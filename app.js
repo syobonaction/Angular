64532a37fadcdb84730b152ea14b1eabfe50a682
syobonaction <smashdaisaku@gmail.com>
2015-09-10 16:42:49 -0500

    Individual Edits
    Fixed to allow editing of individual list items.

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
 var myApp = angular.module('myApp',[]);
 
-
-
-myApp.controller('mainController', function($scope){
-  $scope.people = [];
-  $scope.person = {
-    "name" : "Charles",
-    "age" : 28,
-    "editing" : false
-  }
-  $scope.person2 = {
-    "name" : "Ilker",
-    "age" : 100,
-    "editing" : false
-  }
-  $scope.person3 = {
-    "name" : "William",
-    "age" : 17,
-    "editing" : false
-  }
-
-  $scope.people.push($scope.person);
-  $scope.people.push($scope.person2);
-  $scope.people.push($scope.person3);
-
-  $scope.editMode = function(){
-    if($scope.editing == true) {
-      $scope.editing = false;
-    } else {
-      $scope.editing = true;
-    }
-
-  }
-
+myApp.controller('mainController', function(){
+  this.people = people;
 });
+
+var people = [];
+var person = {
+  "name" : "Charles",
+  "age" : 28,
+  "editing" : false
+}
+var person2 = {
+  "name" : "Ilker",
+  "age" : 100,
+  "editing" : false
+}
+var person3 = {
+  "name" : "William",
+  "age" : 17,
+  "editing" : false
+}
+people.push(person);
+people.push(person2);
+people.push(person3);
